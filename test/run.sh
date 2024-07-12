39ab072364818027d80ebc15f41ee832a898eb20
RonghaiHe <hrhkjys@qq.com>
2024-07-12 14:53:40 +0800

    fix missing node in util_real.launch

--- test/run.sh
+++ test/run.sh
@@ -1,2 +1,3 @@
+id=${1:3}
 source devel/setup.bash
-roslaunch robust_cl_real_world_server util_real.launch --ros-args -param num_robots:=3
+roslaunch robust_cl_real_world_server util_real.launch --ros-args -param num_robots:=${id}
