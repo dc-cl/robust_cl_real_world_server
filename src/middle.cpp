a11e2c4bebb94f0256f397b110c175bed948480f
RonghaiHe <hrhkjys@qq.com>
2024-07-13 17:30:54 +0800

    Modify as queue

--- src/middle.cpp
+++ src/middle.cpp
@@ -3,6 +3,7 @@
 #include "nlink_parser/LinktrackNodeframe2.h"
 #include "cla_monitor/position.h"
 #include <vector>
+#include <queue>
 #include <string>
 #include <cmath>
 #include <memory>
@@ -19,73 +20,115 @@ public:
 
     void callback0(const nlink_parser::LinktrackNodeframe2::ConstPtr& msg) {
         auto pos = msg->pos_3d;
+        // ROS_INFO("Callback0: %f %f %f", pos[0], pos[1], pos[2]);
         std::lock_guard<std::mutex> lock(mutex0_);
-        v_ts0_.emplace_back(msg->system_time);
-        v_pos0_x_.emplace_back(pos[0]);
-        v_pos0_y_.emplace_back(pos[1]);
+        q_ts0_.emplace(msg->system_time);
+        q_pos0_x_.emplace(pos[0]);
+        q_pos0_y_.emplace(pos[1]);
     }
 
     void callback2(const nlink_parser::LinktrackNodeframe2::ConstPtr& msg) {
         auto pos = msg->pos_3d;
         std::lock_guard<std::mutex> lock(mutex2_);
-        v_ts2_.emplace_back(msg->system_time);
-        v_pos2_x_.emplace_back(pos[0]);
-        v_pos2_y_.emplace_back(pos[1]);
+        q_ts2_.emplace(msg->system_time);
+        q_pos2_x_.emplace(pos[0]);
+        q_pos2_y_.emplace(pos[1]);
     }
 
-    void interp1(uint& ts_fir_0, uint& ts_fir_1, uint& ts_0, float& pos_fir_x_0, float& pos_fir_y_0, float& pos_fir_x_1, float& pos_fir_y_1, float& pos_0_x, float& pos_0_y) {
-        uint t_up = ts_0 - ts_fir_0;
-        uint t_down = ts_fir_1 - ts_fir_0;
-        pos_0_x = pos_fir_x_0 + t_up * (pos_fir_x_1 - pos_fir_x_0) / t_down;
-        pos_0_y = pos_fir_y_0 + t_up * (pos_fir_y_1 - pos_fir_y_0) / t_down;
-    }
+    // void interp1(uint& ts_fir_0, uint& ts_fir_1, uint& ts_0, float& pos_fir_x_0, float& pos_fir_y_0, float& pos_fir_x_1, float& pos_fir_y_1, float& pos_0_x, float& pos_0_y) {
+    //     if (abs(ts_fir_0 - ts_0) < epsilon) {
+    //         pos_0_x = pos_fir_x_0;
+    //         pos_0_y = pos_fir_y_0;
+    //     }
+    //     else if (abs(ts_fir_1 - ts_0) < epsilon) {
+    //         pos_0_x = pos_fir_x_1;
+    //         pos_0_y = pos_fir_y_1;
+    //     }
+    //     else {
+    //         uint t_up = ts_0 - ts_fir_0;
+    //         uint t_down = ts_fir_1 - ts_fir_0;
+    //         pos_0_x = pos_fir_x_0 + t_up * (pos_fir_x_1 - pos_fir_x_0) / t_down;
+    //         pos_0_y = pos_fir_y_0 + t_up * (pos_fir_y_1 - pos_fir_y_0) / t_down;
+    //     }
+    // }
 
     void process() {
         std::lock_guard<std::mutex> lock0(mutex0_);
         std::lock_guard<std::mutex> lock2(mutex2_);
-        if (fir < 0 && !v_ts0_.empty() && !v_ts2_.empty()) {
-            if(v_ts0_[0] < v_ts2_[0]) fir = 0;
-            else fir = 2;
+        if (fir < 0 && !q_ts0_.empty() && !q_ts2_.empty()) {
+            if(q_ts0_.front() < q_ts2_.front()) fir = 0;
+            else if(q_ts0_.front() > q_ts2_.front()) fir = 2;
         }
+        if (q_ts0_.empty() || q_ts2_.empty()) return; // No valid data to process
         float pos0[2], pos2[2];
-        if (!fir && !v_pos0_x_.size() > 1 && !v_pos2_x_.empty()) {
-            while(!v_pos0_x_.size() > 1 && v_ts0_[1] < v_ts2_[0]) {
-                v_pos0_x_.pop_back();
-                v_pos0_y_.pop_back();
-                v_ts0_.pop_back();
+        if (!fir && !q_pos0_x_.empty() && !q_pos2_x_.empty()) {
+            while(!q_pos0_x_.empty() && q_ts0_.front() < q_ts2_.front()) {
+                q_pos0_x_.pop();
+                q_pos0_y_.pop();
+                q_ts0_.pop();
             }
-            if (v_pos0_x_.size() > 1) {
-                interp1(v_ts0_[0], v_ts0_[1], v_ts2_[0], v_pos0_x_[0], v_pos0_y_[0], v_pos0_x_[1], v_pos0_y_[1], pos0[0], pos0[1]);
-                pos2[0] = v_pos2_x_[0];
-                pos2[1] = v_pos2_y_[0];
-
-                v_pos0_x_.pop_back();
-                v_pos0_y_.pop_back();
-                v_ts0_.pop_back();
-                v_pos2_x_.pop_back();
-                v_pos2_y_.pop_back();
-                v_ts2_.pop_back();
-            }
+            if(q_pos0_x_.empty()) return;
         }
-        else if (fir == 2 && !v_pos2_x_.size() > 1 && !v_pos0_x_.empty()) {
-            while(!v_pos2_x_.size() > 1 && v_ts2_[1] < v_ts0_[0]) {
-                v_pos2_x_.pop_back();
-                v_pos2_y_.pop_back();
-                v_ts2_.pop_back();
+        else if (fir == 2 && !q_pos2_x_.empty() && !q_pos0_x_.empty()) {
+            while(!q_pos2_x_.empty() && q_ts2_.front() < q_ts0_.front()) {
+                q_pos2_x_.pop();
+                q_pos2_y_.pop();
+                q_ts2_.pop();
             }
-            if (v_pos2_x_.size() > 1) {
-                interp1(v_ts2_[0], v_ts2_[1], v_ts0_[0], v_pos2_x_[0], v_pos2_y_[0], v_pos2_x_[1], v_pos2_y_[1], pos2[0], pos2[1]);
-                pos0[0] = v_pos0_x_[0];
-                pos0[1] = v_pos0_y_[0];
-
-                v_pos0_x_.pop_back();
-                v_pos0_y_.pop_back();
-                v_ts0_.pop_back();
-                v_pos2_x_.pop_back();
-                v_pos2_y_.pop_back();
-                v_ts2_.pop_back();
-            }
+            if(q_pos2_x_.empty()) return;
         }
+        fir = -1;
+
+        pos0[0] = q_pos0_x_.front();
+        pos0[1] = q_pos0_y_.front();
+        pos2[0] = q_pos2_x_.front();
+        pos2[1] = q_pos2_y_.front();
+
+        q_pos0_x_.pop();
+        q_pos0_y_.pop();
+        q_ts0_.pop();
+        q_pos2_x_.pop();
+        q_pos2_y_.pop();
+        q_ts2_.pop();
+
+        // if (!fir && !v_pos0_x_.size() > 1 && !v_pos2_x_.empty()) {
+        //     while(v_pos0_x_.size() > 1 && v_ts0_[1] < v_ts2_[0]) {
+        //         v_pos0_x_.pop();
+        //         v_pos0_y_.pop();
+        //         v_ts0_.pop();
+        //     }
+        //     if (v_pos0_x_.size() > 1) {
+        //         interp1(v_ts0_[0], v_ts0_[1], v_ts2_[0], v_pos0_x_[0], v_pos0_y_[0], v_pos0_x_[1], v_pos0_y_[1], pos0[0], pos0[1]);
+        //         pos2[0] = v_pos2_x_[0];
+        //         pos2[1] = v_pos2_y_[0];
+
+        //         v_pos0_x_.pop();
+        //         v_pos0_y_.pop();
+        //         v_ts0_.pop();
+        //         v_pos2_x_.pop();
+        //         v_pos2_y_.pop();
+        //         v_ts2_.pop();
+        //     }
+        // }
+        // else if (fir == 2 && !v_pos2_x_.size() > 1 && !v_pos0_x_.empty()) {
+        //     while(v_pos2_x_.size() > 1 && v_ts2_[1] < v_ts0_[0]) {
+        //         v_pos2_x_.pop();
+        //         v_pos2_y_.pop();
+        //         v_ts2_.pop();
+        //     }
+        //     if (v_pos2_x_.size() > 1) {
+        //         interp1(v_ts2_[0], v_ts2_[1], v_ts0_[0], v_pos2_x_[0], v_pos2_y_[0], v_pos2_x_[1], v_pos2_y_[1], pos2[0], pos2[1]);
+        //         pos0[0] = v_pos0_x_[0];
+        //         pos0[1] = v_pos0_y_[0];
+
+        //         v_pos0_x_.pop();
+        //         v_pos0_y_.pop();
+        //         v_ts0_.pop();
+        //         v_pos2_x_.pop();
+        //         v_pos2_y_.pop();
+        //         v_ts2_.pop();
+        //     }
+        // }
         cla_monitor::position msg;
         // TODO not just simply in the **middle**
         msg.pos_3d = {(pos0[0] + pos2[0]) / 2, // X
@@ -99,9 +142,9 @@ private:
     int fir; // Judge which uwb sensor comes data first
     ros::Subscriber sub0_, sub2_;
     ros::Publisher pub_;
-    std::vector<float> v_pos0_x_, v_pos2_x_, v_pos0_y_, v_pos2_y_;
-    std::vector<uint> v_ts0_, v_ts2_;
-    std::mutex mutex0_, mutex2_; // Mutexes for the vectors
+    std::queue<float> q_pos0_x_, q_pos2_x_, q_pos0_y_, q_pos2_y_;
+    std::queue<uint> q_ts0_, q_ts2_;
+    std::mutex mutex0_, mutex2_; // Mutexes for the queues
 
 };
 
